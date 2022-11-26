9bcf05d0f17751406b76e92bb2459562cf8bba4e
MARCOSPC\marcos <marcoscobocarrillo@gmail.com>
2022-11-26 12:44:16 +0100

    Patrimonio, valor y deuda por años

--- time_vs_n_prop.py
+++ time_vs_n_prop.py
@@ -1,42 +1,74 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+## Variables piso
 valor_piso = 100000
-valor_reforma = 8000
-entrada_hipoteca = 0.2
-itp = 0.1
-gestion = 0.015
-cap_necesario = valor_piso * (entrada_hipoteca + itp + gestion) + valor_reforma
+cuota_piso = 360
+anios_piso = 30
+deuda_piso = cuota_piso * anios_piso * 12
+valor_reforma_piso = 8000
+entrada_hipoteca_piso = 0.2
+itp_piso = 0.1
+gestion_piso = 0.015
+cap_necesario = valor_piso * (entrada_hipoteca_piso + itp_piso + gestion_piso) + valor_reforma_piso
+cashflow_mes_piso = 100
 
-cashflow_mes = 100
+## Variables simuladas
 cap_mes = 500
-
 num_pisos = 5
+anios_sim = 50
+anios_pasados = 0
+meses_sim = anios_sim * 12
+meses_pasados = 12 * anios_pasados
 
+
+## Calculo de meses necesarios
 meses = []
 for i in range(num_pisos):
-    n = round(cap_necesario / (cap_mes + cashflow_mes * i))
+    n = round(cap_necesario / (cap_mes + cashflow_mes_piso * i))
     meses.append(n)
-
 anios = [round(m / 12, 1) for m in meses]
 
-valores = [valor_piso] * num_pisos
+## Cálculo de valor, deuda y patrimonio acumulado
+valor, valores, deuda, deudas, num_pisos = 0, [], 0, [], 0
+for mes in range(meses[0], meses_sim + 1):
+    if mes in np.cumsum(meses):
+        valor += valor_piso
+        deuda += deuda_piso
+        num_pisos += 1
+    deuda -= cuota_piso * num_pisos
+    valores.append(valor)
+    deudas.append(deuda)
+
+valores = [0] * meses[0] + valores
+deudas = [0] * meses[0] + deudas
+valores = np.array(valores)
+deudas = np.array(deudas)
+deudas[deudas < 0] = 0
+capitales = valores - deudas
 
 
-
+## Gráfico de Número de meses (años) necesarios por piso
 fig, ax = plt.subplots()
 x_labels = ['Piso ' + str(i) for i in range(1, len(meses) + 1)]
 bars = ax.bar(x_labels, meses)
-ax.set_title('Número de meses (años) necesarios por número de pisos')
+ax.set_title('Número de meses (años) necesarios por piso')
 ax.bar_label(bars, label_type='edge', padding=3)
 ax.bar_label(bars, anios, label_type='edge', padding=-16, color='white')
 ax.margins(y=0.1)
 fig.tight_layout()
 plt.show()
 
-
+## Gráfico de Patrimonio (€) por años
 fig, ax = plt.subplots()
-line = ax.plot(np.cumsum([0] + anios), np.cumsum([0] + valores), 'o--')
+years = np.arange(0, anios_sim + 1 - anios_pasados, 5)
+years_starts = [y * 12 + meses_pasados for y in years]
+line = ax.step(range(meses_sim + 1)[meses_pasados:], valores[meses_pasados:], '--', label='valor')
+line = ax.step(range(meses_sim + 1)[meses_pasados:], deudas[meses_pasados:], '--', label='deuda')
+line = ax.step(range(meses_sim + 1)[meses_pasados:], capitales[meses_pasados:], '--', label='patrimonio')
+ax.set_title('Patrimonio (€) por años')
+ax.legend()
+ax.set_xticks(years_starts)
+ax.set_xticklabels(years)
 fig.tight_layout()
 plt.show()

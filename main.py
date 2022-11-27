cd043a1f8adeab71461ec7eec34c94404a3b60df
MARCOSPC\marcos <marcoscobocarrillo@gmail.com>
2022-11-27 11:05:59 +0100

    hotfix hecho

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ interes_anual = 0.05
 cap_mes_inmuebles = 500
 cap_mes_renta_variable = 500
 
-simular_patrimonio(anios_sim, num_pisos, interes_anual, cap_mes_inmuebles, cap_mes_renta_variable,
-                   save='figure.png')
+save = 'export/export_{}y{}p{}i{}ci{}crv.png'.format(anios_sim, num_pisos, int(interes_anual * 100),
+                                                     cap_mes_inmuebles, cap_mes_renta_variable)
+
+simular_patrimonio(anios_sim, num_pisos, interes_anual, cap_mes_inmuebles, cap_mes_renta_variable, save=save)
